9e925e38329893fa12f76bfdf632aed156148c11
Aaron Germuth <germuth@unbc.ca>
2015-03-17 18:29:46 -0700

    finished about page and dynamic page titles

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,23 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
-  test "should get home" do
-    get :home
+	test "should get home" do
+		get :home
+		assert_response :success
+		assert_select	"title", "Ruby on Rails Sample App"
+	end
+
+	test "should get help" do
+		get :help
     assert_response :success
+		assert_select	"title", "Help | Ruby on Rails Sample App"
   end
 
-  test "should get help" do
-    get :help
-    assert_response :success
-  end
+  test "should get about" do
+		get :about
+		assert_response :success
+		assert_select	"title", "About | Ruby on Rails Sample App"
+	end
+
 
 end
